dda2c4f2387036dc885117cb5723701875fc8fa6
dchagastelles <dchagastelles@gmail.com>
2021-05-04 08:32:40 -0400

    - Add endpoint to access github issues

---
+++ src/tests/support/issues.sh
@@ -0,0 +1,1 @@
+curl -XPOST -H "Content-Type: application/json" -H "Authorization: profile-api-secret-key" -d '{}' http://localhost:8082/v1/support_router/github/dchagastelles/profile-service-nodejs-api/issues
--- src/v1/support_router.ts
+++ src/v1/support_router.ts
@@ -26,4 +26,23 @@ supportRouter.post("/send_email", async (req, res) => {
   returnSuccess(res, {});
 });
 
+
+supportRouter.post("/github/:owner/:repos/issues", async (req, res) => {
+  // tslint:disable-next-line:no-console
+  console.info("Executing: /v1/support_router/github/issues");
+
+  const postData: any = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'Authorization': 'Basic ' + Buffer.from(`${process.env.GITHUB_API_USER}:${process.env.GITHUB_API_TOKEN}`).toString('base64')
+    }
+  };
+
+  const fetchResponse = await fetch(`https://api.github.com/repos/${req.params.owner}/${req.params.repos}/issues`, postData);
+  const response = await fetchResponse.json();
+  
+  returnSuccess(res, response);
+});
+
 export default supportRouter;
