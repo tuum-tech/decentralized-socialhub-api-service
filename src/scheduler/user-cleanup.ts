c21331a3ac5649c779f81ac846d9d449ed2228bf
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-10-28 14:58:17 +0000

    Fixed issues

--- src/scheduler/user-cleanup.ts
+++ src/scheduler/user-cleanup.ts
@@ -1,40 +1,42 @@
-import { IRunScriptData, IRunScriptResponse } from "@elastos/elastos-hive-js-sdk/dist/Services/Scripting.Service";
+import {
+  IRunScriptData,
+  IRunScriptResponse,
+} from "@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service";
 import cron from "node-cron";
 import { getHiveClient, getNonAnonymousClient } from "../v1/commom";
 
-
 export function scheduleUsersCleanUp() {
+  cron.schedule("0 * * * *", async () => {
+    // tslint:disable-next-line:no-console
+    console.log("Running user cleanup");
 
-    cron.schedule('0 * * * *', async () => {
-        // tslint:disable-next-line:no-console
-        console.log('Running user cleanup');
+    const hiveClient = await getHiveClient();
 
-        const hiveClient = await getHiveClient();
+    const now = new Date();
+    const hour = 1000 * 60 * 60; // an hour
 
-        const now = new Date()
-        const hour = 1000 * 60 * 60 // an hour
+    const cutoffDate = new Date(now.getTime() - hour);
+    const expiredTimestamp = now.getTime() - hour;
 
-        const cutoffDate = new Date(now.getTime() - hour);
-        const expiredTimestamp = now.getTime() - hour;
+    // tslint:disable-next-line:no-console
+    console.log(`${expiredTimestamp}`);
 
-        // tslint:disable-next-line:no-console
-        console.log(`${expiredTimestamp }`);
+    const deleteScript = {
+      name: "delete_expired_users",
+      params: {
+        timestamp: expiredTimestamp,
+      },
+      context: {
+        target_did: `${process.env.TUUMVAULT_DID}`,
+        target_app_did: `${process.env.TUUMVAULT_DID}`,
+      },
+    };
+    const response: IRunScriptResponse<any> =
+      await hiveClient.Scripting.RunScript<any>(deleteScript as IRunScriptData);
 
-        const deleteScript = {
-            name: 'delete_expired_users',
-            params: {
-                timestamp: expiredTimestamp
-            },
-            context: {
-              target_did: `${process.env.TUUMVAULT_DID}`,
-              target_app_did: `${process.env.TUUMVAULT_DID}`
-            }
-          };
-        const response : IRunScriptResponse<any> =  await hiveClient.Scripting.RunScript<any>(deleteScript as IRunScriptData);
-
-        if (!response.isSuccess){
-            // tslint:disable-next-line:no-console
-            console.error(`Could not cleanup users. Error: ${response.error}`);
-        }
-    });
+    if (!response.isSuccess) {
+      // tslint:disable-next-line:no-console
+      console.error(`Could not cleanup users. Error: ${response.error}`);
+    }
+  });
 }
--- src/v1/tuumvault_router.ts
+++ src/v1/tuumvault_router.ts
@@ -1,46 +1,58 @@
-import express, { request } from 'express';
-import { IRunScriptData, IRunScriptResponse, ISetScriptData, ISetScriptResponse } from '@elastos/elastos-hive-js-sdk/dist/Services/Scripting.Service';
-import { getHiveClient, getToken, handleHiveResponse, handleRoute, returnSuccess } from './commom';
+import express, { request } from "express";
+import {
+  IRunScriptData,
+  IRunScriptResponse,
+  ISetScriptData,
+  ISetScriptResponse,
+} from "@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service";
+import {
+  getHiveClient,
+  getToken,
+  handleHiveResponse,
+  handleRoute,
+  returnSuccess,
+} from "./commom";
 
 const tuumvaultRouter = express.Router();
 
-
-tuumvaultRouter.post('/scripting/set_script', async (req, res) => {
-    const hiveClient = await getHiveClient();
-    const response : ISetScriptResponse =  await hiveClient.Scripting.SetScript(req.body as ISetScriptData);
-    returnSuccess(res, JSON.stringify(response));
+tuumvaultRouter.post("/scripting/set_script", async (req, res) => {
+  const hiveClient = await getHiveClient();
+  const response: ISetScriptResponse = await hiveClient.Scripting.SetScript(
+    req.body as ISetScriptData
+  );
+  returnSuccess(res, JSON.stringify(response));
 });
 
-tuumvaultRouter.post('/scripting/run_script', async (req, res) => {
-    // tslint:disable-next-line:no-console
-    console.info("Executing: /v1/scripting/run_script");
+tuumvaultRouter.post("/scripting/run_script", async (req, res) => {
+  // tslint:disable-next-line:no-console
+  console.info("Executing: /v1/scripting/run_script");
 
-    const hiveClient = await getHiveClient();
-    const response : IRunScriptResponse<any> =  await hiveClient.Scripting.RunScript<any>(req.body as IRunScriptData);
+  const hiveClient = await getHiveClient();
+  const response: IRunScriptResponse<any> =
+    await hiveClient.Scripting.RunScript<any>(req.body as IRunScriptData);
 
-    returnSuccess(res, response.response);
+  returnSuccess(res, response.response);
 });
 
-tuumvaultRouter.post('/scripting/run_script_upload/:transaction_id', async (req, res) => {
-
+tuumvaultRouter.post(
+  "/scripting/run_script_upload/:transaction_id",
+  async (req, res) => {
     const url = `${process.env.TUUMVAULT_API_URL}/api/v1/scripting/run_script_upload/{transaction_id}`;
     const token = await getToken();
 
     const postData: any = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'Authorization': `token ${token}`
-        }
-      };
-
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `token ${token}`,
+      },
+    };
 
     const fetchResponse = await fetch(url, postData);
     res.send(JSON.stringify(fetchResponse));
 
-   // const ret: any = await handleRoute(url, req.body, getDidcredsHeader(), false);
-});
-
-
+    // const ret: any = await handleRoute(url, req.body, getDidcredsHeader(), false);
+  }
+);
 
 export default tuumvaultRouter;
