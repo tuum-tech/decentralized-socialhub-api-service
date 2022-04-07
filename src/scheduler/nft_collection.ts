42e2aacb77c79c44372154b6cd44c5de840d6438
eratos <37297413+Eratos1122@users.noreply.github.com>
2022-04-07 18:51:07 +0900

    moralis api integration

--- src/scheduler/nft_collection.ts
+++ src/scheduler/nft_collection.ts
@@ -1,79 +1,90 @@
-import { BigNumber } from '@ethersproject/bignumber'
+import { BigNumber } from "@ethersproject/bignumber";
 import {
   IRunScriptData,
   IRunScriptResponse,
-} from '@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service'
-import cron from 'node-cron'
-import { getHiveClient, getNonAnonymousClient } from '../v1/common'
-import mockData from './nft_collection_assets.json'
+} from "@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service";
+import cron from "node-cron";
+import { getHiveClient, getNonAnonymousClient } from "../v1/common";
 
 export function scheduleNFTCollectionAssetsUpdate() {
-  cron.schedule('*/10 * * * *', async () => {
-    const hiveClient = await getHiveClient()
+  cron.schedule("*/10 * * * *", async () => {
+    const hiveClient = await getHiveClient();
     const response: IRunScriptResponse<any> =
       await hiveClient.Scripting.RunScript<any>({
-        name: 'get_nft_collection_spaces',
+        name: "get_nft_collection_spaces",
         context: {
           target_did: `${process.env.TUUMVAULT_DID}`,
           target_app_did: `${process.env.TUUMVAULT_DID}`,
         },
-      })
-    if (response.response._status !== 'OK') return
+      });
+    if (response.response._status !== "OK") return;
     const collections = Promise.all(
       await response.response.get_nft_collection_spaces.items.map(
         async (space: any) => {
-          const { network, slug, address } = space.meta
-          const openseaAPIUrl = `https://api.opensea.io/api/v1/assets?collection_slug=${slug}`
-          const elacityAPIUrl = 'https://ela.city/api/nftitems/fetchTokens'
-          // const url =
-          //   "https://api.opensea.io/api/v1/collections?offset=0&limit=5";
-          let assets: any[] = []
-
-          if (network.toLowerCase() === 'ethereum') {
-            // const result = await fetch(openseaAPIUrl);
-            // const { status, statusText } = result;
-            const status = 200
-            const statusText = 'OK'
-            const data = mockData
-
-            if (status === 200 && statusText === 'OK') {
-              // const data = await result.json();
-              assets = data.assets.map((asset: any) => ({
-                name: asset.name,
-                image_url: asset.image_url,
-                owner: asset.owner.address,
-                last_sale: asset.last_sale
-                  ? {
-                      price: BigNumber.from(asset.last_sale.total_price)
-                        .div(
-                          BigNumber.from(10).pow(
-                            BigNumber.from(
-                              asset.last_sale.payment_token.decimals
-                            )
-                          )
-                        )
-                        .toNumber(),
-                      token: 'ETH',
-                    }
-                  : null,
-              }))
+          const { network, slug, address } = space.meta;
+          let assets: any[] = [];
+          // tslint:disable-next-line:no-console
+          console.log(
+            `==============Starting To Get ${slug} Collection Assets================`
+          );
+          if (network.toLowerCase() === "ethereum") {
+            let cursor = "";
+            try {
+              do {
+                const moralisAPIUrl = `https://deep-index.moralis.io/api/v2/nft/${address}/owners?chain=eth&format=decimal&cursor=${cursor}`;
+                const result = await fetch(moralisAPIUrl, {
+                  method: "GET",
+                  headers: {
+                    "x-api-key": process.env.MORALIS_API_KEY,
+                  },
+                });
+                const { status, statusText } = result;
+                if (status === 200 && statusText === "OK") {
+                  const data = await result.json();
+                  cursor = data.cursor;
+                  assets = assets.concat(
+                    data.result.map((asset: any) => {
+                      const { image, name } = JSON.parse(asset.metadata);
+                      return {
+                        name,
+                        image_url: image,
+                        owner: asset.owner_of,
+                        last_sale: asset.last_sale,
+                      };
+                    })
+                  );
+                  // tslint:disable-next-line:no-console
+                  console.log(
+                    data.page,
+                    data.page_size,
+                    data.result.length,
+                    assets.length
+                  );
+                } else {
+                  cursor = "";
+                }
+              } while (cursor !== "");
+            } catch (error) {
+              // tslint:disable-next-line:no-console
+              console.log(error);
             }
           } else {
+            const elacityAPIUrl = "https://ela.city/api/nftitems/fetchTokens";
             const result = await fetch(elacityAPIUrl, {
-              method: 'POST',
+              method: "POST",
               headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
               },
               body: JSON.stringify({
-                type: 'single',
-                sortby: 'createdAt$desc',
+                type: "single",
+                sortby: "createdAt$desc",
                 collectionAddresses: [address],
               }),
-            })
-            const { status, statusText } = result
-            if (status === 200 && statusText === 'OK') {
-              const data = await result.json()
-              if (data.status === 'success') {
+            });
+            const { status, statusText } = result;
+            if (status === 200 && statusText === "OK") {
+              const data = await result.json();
+              if (data.status === "success") {
                 assets = data.data.tokens.map((asset: any) => ({
                   name: asset.name,
                   image_url: asset.imageURL,
@@ -81,26 +92,26 @@ export function scheduleNFTCollectionAssetsUpdate() {
                   last_sale: asset.price
                     ? {
                         price: asset.price,
-                        token: 'ELA',
+                        token: "ELA",
                       }
                     : null,
-                }))
+                }));
               }
             }
           }
 
           if (assets.length > 0) {
             await hiveClient.Scripting.RunScript<any>({
-              name: 'update_nft_collection_assets',
+              name: "update_nft_collection_assets",
               params: { guid: space.guid, assets },
               context: {
                 target_did: `${process.env.TUUMVAULT_DID}`,
                 target_app_did: `${process.env.TUUMVAULT_DID}`,
               },
-            })
+            });
           }
         }
       )
-    )
-  })
+    );
+  });
 }
