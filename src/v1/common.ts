6659e526540b7bd67940b148a828a7b023194c74
conlot-crypto <denis.vasilev@yahoo.com>
2022-04-28 23:02:52 +1000

    fix referal

--- src/v1/common.ts
+++ src/v1/common.ts
@@ -381,7 +381,7 @@ export async function registerVerifyAttempt(
       hiveHost: "",
       avatar: "",
       timestamp: Date.now(),
-      referals: [] as string[]
+      referrals: [] as any[]
     },
   };
 
