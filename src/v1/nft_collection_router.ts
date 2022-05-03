2cbfa96f9d8567e9415ffbaed4bb02752d368501
eratos <37297413+Eratos1122@users.noreply.github.com>
2022-05-04 00:21:25 +0900

    standarize fetching nft item owners

--- src/v1/nft_collection_router.ts
+++ src/v1/nft_collection_router.ts
@@ -1,40 +1,40 @@
-import express from 'express'
-import _ from 'lodash'
+import express from "express";
+import _ from "lodash";
 import {
   IRunScriptData,
   IRunScriptResponse,
-} from '@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service'
-import { returnSuccess, getHiveClient, returnError } from './common'
+} from "@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service";
+import { returnSuccess, getHiveClient, returnError } from "./common";
 
-const NFTCollectionRouter = express.Router()
+const NFTCollectionRouter = express.Router();
 
-NFTCollectionRouter.get('/assets', async (req, res) => {
+NFTCollectionRouter.get("/assets", async (req, res) => {
   // tslint:disable-next-line:no-console
-  console.info('Executing: /v1/nft_collection_router/assets')
+  console.info("Executing: /v1/nft_collection_router/assets");
 
-  const { collection_id, offset, limit } = req.query
-  const hiveClient = await getHiveClient()
+  const { collection_id, offset, limit } = req.query;
+  const hiveClient = await getHiveClient();
   const response: IRunScriptResponse<any> =
     await hiveClient.Scripting.RunScript<any>({
-      name: 'get_nft_collection_assets',
+      name: "get_nft_collection_assets",
       params: { guid: JSON.parse(collection_id as string) },
       context: {
         target_did: `${process.env.TUUMVAULT_DID}`,
         target_app_did: `${process.env.TUUMVAULT_DID}`,
       },
-    })
-  if (response.response._status !== 'OK') {
-    returnError(res, 'Tuum tech vault script error')
+    });
+  if (response.response._status !== "OK") {
+    returnError(res, "Tuum tech vault script error");
   }
-  const collections = response.response.get_nft_collection_assets.items
+  const collections = response.response.get_nft_collection_assets.items;
 
   if (collections.length > 0) {
-    const { assets } = collections[0]
+    const { assets } = collections[0];
 
-    const groups = _.groupBy(assets, 'name')
+    const groups = _.groupBy(assets, "name");
     const flattenAssets = Object.keys(groups).map(
       (name: string) => groups[name][0]
-    )
+    );
 
     returnSuccess(res, {
       totalCount: flattenAssets.length,
@@ -44,46 +44,52 @@ NFTCollectionRouter.get('/assets', async (req, res) => {
         offset as any,
         parseInt(offset as string, 10) + parseInt(limit as string, 10)
       ),
-    })
+    });
   } else {
-    returnError(res, 'No collection exist')
+    returnError(res, "No collection exist");
   }
-})
+});
 
-NFTCollectionRouter.get('/owners', async (req, res) => {
+NFTCollectionRouter.get("/owners", async (req, res) => {
   // tslint:disable-next-line:no-console
-  console.info('Executing: /v1/nft_collection_router/owners')
+  console.info("Executing: /v1/nft_collection_router/owners");
 
-  const { collection_id, offset, limit } = req.query
-  const hiveClient = await getHiveClient()
+  const { collection_id, offset, limit } = req.query;
+  const hiveClient = await getHiveClient();
   const response: IRunScriptResponse<any> =
     await hiveClient.Scripting.RunScript<any>({
-      name: 'get_nft_collection_assets',
+      name: "get_nft_collection_assets",
       params: { guid: JSON.parse(collection_id as string) },
       context: {
         target_did: `${process.env.TUUMVAULT_DID}`,
         target_app_did: `${process.env.TUUMVAULT_DID}`,
       },
-    })
-  if (response.response._status !== 'OK') {
-    returnError(res, 'Tuum tech vault script error')
+    });
+  if (response.response._status !== "OK") {
+    returnError(res, "Tuum tech vault script error");
   }
-  const collections = response.response.get_nft_collection_assets.items
+  const collections = response.response.get_nft_collection_assets.items;
   if (collections.length > 0) {
-    const { assets } = collections[0]
-    const owners = [...new Set(assets.map((asset: any) => asset.owner))]
+    const { assets } = collections[0];
+    const owners = [...new Set(assets.map((asset: any) => asset.owner))].filter(
+      (owner) => owner
+    );
     returnSuccess(res, {
       totalCount: owners.length,
       offset,
       limit,
-      owners: owners.slice(
-        offset as any,
-        parseInt(offset as string, 10) + parseInt(limit as string, 10)
-      ),
-    })
+      owners:
+        parseInt(offset as string, 10) !== 0 ||
+        parseInt(limit as string, 10) !== 0
+          ? owners.slice(
+              offset as any,
+              parseInt(offset as string, 10) + parseInt(limit as string, 10)
+            )
+          : owners,
+    });
   } else {
-    returnError(res, 'No collection exist')
+    returnError(res, "No collection exist");
   }
-})
+});
 
-export default NFTCollectionRouter
+export default NFTCollectionRouter;
