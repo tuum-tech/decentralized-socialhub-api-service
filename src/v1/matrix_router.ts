98836f27dc6258d964446f7ab936ebcda3073ece
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-05-20 15:49:40 -0400

    Better handling of errors

--- src/v1/matrix_router.ts
+++ src/v1/matrix_router.ts
@@ -14,32 +14,37 @@ matrixRouter.post('/auth', async (req, res) => {
   const did = req.body.did
   const url = `${process.env.SYNAPSE_API_URL}`
   const client = createClient(url)
-  const userLogin = did.replace('/did:elastos:/g', '')
+  const userLogin = did.replace('did:elastos:', '')
   const password = bcrypt.hashSync(`${did}_${process.env.TUUMVAULT_DID}`, 10)
 
-  await client.login('m.login.password', {
-    user: `${process.env.SYNAPSE_ROOT_USER}`,
-    password: `${process.env.SYNAPSE_ROOT_PASSWORD}`,
-  })
-
+  let accessToken = ''
   try {
+    await client.login('m.login.password', {
+      user: `${process.env.SYNAPSE_ROOT_USER}`,
+      password: `${process.env.SYNAPSE_ROOT_PASSWORD}`,
+    })
+
     // TODO: verify/save on Profile hive users that already registered on Synapse
     await client.register(userLogin, password, '', { type: '' })
+    const userClient = createClient(url)
+    const responseUserLogin = await userClient.login('m.login.password', {
+      user: userLogin,
+      password,
+    })
+
+    accessToken = responseUserLogin.access_token
   } catch (error) {
     // TODO: Verify what kind of error before continue
+    // tslint:disable-next-line:no-console
+    console.log(
+      `Error while trying to register profile user "${userLogin}": ${error.toString()}`
+    )
   }
-
-  const userClient = createClient(url)
-
-  const responseUserLogin = await userClient.login('m.login.password', {
-    user: userLogin,
-    password,
-  })
 
   const ret: any = await handleRoute(
     url,
     {
-      access_token: responseUserLogin.access_token,
+      access_token: accessToken,
     },
     {},
     true
