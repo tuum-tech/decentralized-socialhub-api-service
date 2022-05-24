c60fc49a52df3f46f5e7365b4d10082a073f3ed1
Ricardo Trapp <ricardo.trapp@gmail.com>
2022-05-23 20:13:17 -0400

    Fixing matrix authentication

--- src/v1/matrix_router.ts
+++ src/v1/matrix_router.ts
@@ -11,46 +11,60 @@ interface IMatrixAuth {
 matrixRouter.post('/auth', async (req, res) => {
   // tslint:disable-next-line:no-console
   console.info('Executing: /v1/matrix_router/auth')
-  const did = req.body.did
+  const did = req.body.did as string
   const url = `${process.env.SYNAPSE_API_URL}`
   const client = createClient(url)
-  const userLogin = did.replace('did:elastos:', '')
-  const password = bcrypt.hashSync(`${did}_${process.env.TUUMVAULT_DID}`, 10)
+  const userLogin = did.replace('did:elastos:', '').toLowerCase()
+  const password = bcrypt.hashSync(`${did}_${process.env.TUUMVAULT_DID}`, process.env.SYNAPSE_PASSWORD_SALT)
 
   let accessToken = ''
+
+  let createNewUser = false;
+
   try {
-    await client.login('m.login.password', {
-      user: `${process.env.SYNAPSE_ROOT_USER}`,
-      password: `${process.env.SYNAPSE_ROOT_PASSWORD}`,
-    })
+    
+    const responseUserLogin = await client.login('m.login.password', {
+        user: userLogin,
+        password,
+      })
 
-    // TODO: verify/save on Profile hive users that already registered on Synapse
-    await client.register(userLogin, password, '', { type: '' })
-    const userClient = createClient(url)
-    const responseUserLogin = await userClient.login('m.login.password', {
-      user: userLogin,
-      password,
-    })
+      accessToken = responseUserLogin.access_token
 
-    accessToken = responseUserLogin.access_token
   } catch (error) {
-    // TODO: Verify what kind of error before continue
-    // tslint:disable-next-line:no-console
-    console.log(
-      `Error while trying to register profile user "${userLogin}": ${error.toString()}`
-    )
+      createNewUser = true
   }
 
-  const ret: any = await handleRoute(
-    url,
-    {
-      access_token: accessToken,
-    },
-    {},
-    true
-  )
 
-  returnSuccess(res, ret)
+  if (createNewUser){
+    try {
+        await client.login('m.login.password', {
+          user: `${process.env.SYNAPSE_ROOT_USER}`,
+          password: `${process.env.SYNAPSE_ROOT_PASSWORD}`,
+        })
+
+        await client.register(userLogin, password, '', { type: 'm.login.dummy' })
+        const userClient = createClient(url)
+        const responseUserLogin = await userClient.login('m.login.password', {
+          user: userLogin,
+          password,
+        })
+
+        accessToken = responseUserLogin.access_token
+    
+      } catch (error) {
+        // TODO: Verify what kind of error before continue
+        // tslint:disable-next-line:no-console
+        console.log(
+          `Error while trying to register profile user "${userLogin}": ${error.toString()}`
+        )
+      }
+  }
+
+  const response = {
+    access_token: accessToken,
+  }
+
+  returnSuccess(res, response)
 })
 
 export default matrixRouter
