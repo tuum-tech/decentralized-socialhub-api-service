a6354c908a6a1e3b5563a8f3db8daa6df2270bfe
eratos <37297413+Eratos1122@users.noreply.github.com>
2022-05-27 20:36:40 +0900

    add opensea api for nft collection on opensea

--- src/scheduler/nft_collection.ts
+++ src/scheduler/nft_collection.ts
@@ -1,150 +1,202 @@
-import { BigNumber } from '@ethersproject/bignumber'
-import {
-  IRunScriptData,
-  IRunScriptResponse,
-} from '@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service'
-import cron from 'node-cron'
-import { getHiveClient, getNonAnonymousClient } from '../v1/common'
+import { IRunScriptResponse } from "@elastosfoundation/elastos-hive-js-sdk/dist/Services/Scripting.Service";
+import cron from "node-cron";
+import BigNumber from "bignumber.js";
+import { getHiveClient } from "../v1/common";
 
+const getAssetsUsingElacityAPI = async (address: string) => {
+  const elacityAPIUrl = "https://ela.city/api/nftitems/fetchTokens";
+  const result = await fetch(elacityAPIUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      type: "single",
+      sortby: "createdAt$desc",
+      collectionAddresses: [address],
+    }),
+  });
+  const { status, statusText } = result;
+  if (status === 200 && statusText === "OK") {
+    const data = await result.json();
+    if (data.status === "success") {
+      return data.data.tokens.map((asset: any) => ({
+        name: asset.name,
+        image_url: asset.imageURL,
+        owner: asset.owner?.address,
+        last_sale: asset.price
+          ? {
+              price: asset.price,
+              token: "ELA",
+            }
+          : null,
+      }));
+    }
+  }
+  return [];
+};
+const getAssetsUsingMoralisAPI = async (address: string, chain: string) => {
+  let cursor = "";
+  let assets: any[] = [];
+  do {
+    const moralisAPIUrl = `https://deep-index.moralis.io/api/v2/nft/${address}/owners?chain=${chain}&format=decimal&cursor=${cursor}`;
+    const result = await fetch(moralisAPIUrl, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "X-API-Key": process.env.MORALIS_API_KEY,
+      },
+    });
+    const { status, statusText } = result;
+    if (status === 200 && statusText === "OK") {
+      const data = await result.json();
+      cursor = data.cursor;
+      assets = assets.concat(
+        data.result.map((asset: any) => {
+          if (typeof asset === "object") {
+            const name = `${asset.name} #${asset.token_id}`;
+            let imageUrl = "";
+            const metadata = JSON.parse(asset.metadata);
+            if (metadata !== null) {
+              imageUrl = metadata.image;
+            }
+            return {
+              name,
+              image_url: imageUrl,
+              owner: asset.owner_of,
+              last_sale: asset.last_sale,
+            };
+          }
+        })
+      );
+      // tslint:disable-next-line:no-console
+      console.log(data.page, data.page_size, data.result.length, assets.length);
+    } else {
+      cursor = "";
+    }
+  } while (cursor !== "");
+  return assets;
+};
+const getAssetsUsingOpenseaAPI = async (slug: string) => {
+  let cursor = "";
+  let assets: any[] = [];
+  const zeroAddress = "0x0000000000000000000000000000000000000000";
+  try {
+    do {
+      const openseaAPIUrl = `https://api.opensea.io/api/v1/assets?collection_slug=${slug}&cursor=${cursor}&limit=50`;
+      const result = await fetch(openseaAPIUrl, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          "X-API-Key": process.env.OPENSEA_API_KEY,
+        },
+      });
+      const { status, statusText } = result;
+      if (status === 200 && statusText === "OK") {
+        const data = await result.json();
+        cursor = data.next;
+        assets = assets.concat(
+          data.assets.map((asset: any) => {
+            const { name, image_url, owner, creator, last_sale } = asset;
+            return {
+              name,
+              image_url,
+              owner:
+                owner.address === zeroAddress ? creator.address : owner.address,
+              last_sale: last_sale
+                ? {
+                    token: last_sale.payment_token.symbol,
+                    price: new BigNumber(last_sale.total_price)
+                      .dividedBy(
+                        new BigNumber(10).pow(last_sale.payment_token.decimals)
+                      )
+                      .toNumber(),
+                  }
+                : null,
+            };
+          })
+        );
+        // tslint:disable-next-line:no-console
+        console.log(cursor, 50, data.assets.length, assets.length);
+      } else {
+        cursor = "";
+      }
+    } while (cursor !== "");
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.log(error);
+  }
+  return assets;
+};
 export function scheduleNFTCollectionAssetsUpdate() {
-  cron.schedule('*/10 * * * *', async () => {
-    const hiveClient = await getHiveClient()
+  cron.schedule("*/10 * * * *", async () => {
+    const hiveClient = await getHiveClient();
     const response: IRunScriptResponse<any> =
       await hiveClient.Scripting.RunScript<any>({
-        name: 'get_nft_collection_spaces',
+        name: "get_nft_collection_spaces",
         context: {
           target_did: `${process.env.TUUMVAULT_DID}`,
           target_app_did: `${process.env.TUUMVAULT_DID}`,
         },
-      })
-    if (response.response._status !== 'OK') return
+      });
+    if (response.response._status !== "OK") return;
     // tslint:disable-next-line:no-console
     console.log(
-      '=============== Starting To Get Collection Assets ================',
-      '\n'
-    )
+      "=============== Starting To Get Collection Assets ================",
+      "\n"
+    );
     const collections = await Promise.all(
       await response.response.get_nft_collection_spaces.items.map(
         async (space: any) => {
-          const { network, slug, address } = space.meta
-          let assets: any[] = []
+          const { network, address, isOpenseaCollection, slug } = space.meta;
+          let assets: any[] = [];
           // tslint:disable-next-line:no-console
-          console.log(`fetching *${slug}* ...`)
-          if (network.toLowerCase() === 'elastos smart contract chain') {
-            const elacityAPIUrl = 'https://ela.city/api/nftitems/fetchTokens'
-            const result = await fetch(elacityAPIUrl, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                type: 'single',
-                sortby: 'createdAt$desc',
-                collectionAddresses: [address],
-              }),
-            })
-            const { status, statusText } = result
-            if (status === 200 && statusText === 'OK') {
-              const data = await result.json()
-              if (data.status === 'success') {
-                assets = data.data.tokens.map((asset: any) => ({
-                  name: asset.name,
-                  image_url: asset.imageURL,
-                  owner: asset.owner?.address,
-                  last_sale: asset.price
-                    ? {
-                        price: asset.price,
-                        token: 'ELA',
-                      }
-                    : null,
-                }))
+          console.log(`fetching *${space.name}* ...`);
+          if (network.toLowerCase() === "elastos smart contract chain") {
+            assets = await getAssetsUsingElacityAPI(address);
+          } else {
+            // use Opensea API
+            if (isOpenseaCollection) {
+              assets = await getAssetsUsingOpenseaAPI(slug);
+            }
+            // Else use Moralis API
+            else {
+              let chain = "";
+              // Available values for chains: eth, 0x1, ropsten, 0x3, rinkeby, 0x4, goerli, 0x5, kovan, 0x2a
+              /// polygon, 0x89, mumbai, 0x13881, bsc, 0x38, bsc testnet, 0x61, avalance, 0xa86a, avalanche testnet
+              // 0xa869, fantom, 0xfa
+              if (network.toLowerCase() === "ethereum") {
+                chain = "eth";
+              } else if (network.toLowerCase() === "polygon") {
+                chain = "polygon";
+              } else {
+                // tslint:disable-next-line:no-console
+                console.log(`${network} not currently supported`);
+                return;
               }
-            }
-          }
-          // Else use Moralis API
-          else {
-            let chain = ''
-            // Available values for chains: eth, 0x1, ropsten, 0x3, rinkeby, 0x4, goerli, 0x5, kovan, 0x2a
-            /// polygon, 0x89, mumbai, 0x13881, bsc, 0x38, bsc testnet, 0x61, avalance, 0xa86a, avalanche testnet
-            // 0xa869, fantom, 0xfa
-            if (network.toLowerCase() === 'ethereum') {
-              chain = 'eth'
-            } else if (network.toLowerCase() === 'polygon') {
-              chain = 'polygon'
-            } else {
-              // tslint:disable-next-line:no-console
-              console.log(`${network} not currently supported`)
-              return
-            }
-            let cursor = ''
-            try {
-              do {
-                const moralisAPIUrl = `https://deep-index.moralis.io/api/v2/nft/${address}/owners?chain=${chain}&format=decimal&cursor=${cursor}`
-                const result = await fetch(moralisAPIUrl, {
-                  method: 'GET',
-                  headers: {
-                    accept: 'application/json',
-                    'X-API-Key': process.env.MORALIS_API_KEY,
-                  },
-                })
-                const { status, statusText } = result
-                if (status === 200 && statusText === 'OK') {
-                  const data = await result.json()
-                  cursor = data.cursor
-                  assets = assets.concat(
-                    data.result.map((asset: any) => {
-                      if (typeof asset === 'object') {
-                        const name = `${asset.name} #${asset.token_id}`
-                        let imageUrl = ''
-                        const metadata = JSON.parse(asset.metadata)
-                        if (metadata !== null) {
-                          imageUrl = metadata.image
-                        }
-                        return {
-                          name,
-                          image_url: imageUrl,
-                          owner: asset.owner_of,
-                          last_sale: asset.last_sale,
-                        }
-                      }
-                    })
-                  )
-                  // tslint:disable-next-line:no-console
-                  console.log(
-                    data.page,
-                    data.page_size,
-                    data.result.length,
-                    assets.length
-                  )
-                } else {
-                  cursor = ''
-                }
-              } while (cursor !== '')
-            } catch (error) {
-              // tslint:disable-next-line:no-console
-              console.log(error)
+              assets = await getAssetsUsingMoralisAPI(address, chain);
             }
           }
 
           if (assets.length > 0) {
             await hiveClient.Scripting.RunScript<any>({
-              name: 'update_nft_collection_assets',
+              name: "update_nft_collection_assets",
               params: { guid: space.guid, assets },
               context: {
                 target_did: `${process.env.TUUMVAULT_DID}`,
                 target_app_did: `${process.env.TUUMVAULT_DID}`,
               },
-            })
+            });
             // tslint:disable-next-line:no-console
-            console.log(`all *${slug}* retrieved.`)
+            console.log(`all *${space.name}* retrieved.`);
           }
         }
       )
-    )
+    );
     // tslint:disable-next-line:no-console
     console.log(
-      '\n',
-      '============================ Complete ============================'
-    )
-  })
+      "\n",
+      "============================ Complete ============================"
+    );
+  });
 }
